1c84157a8cd5bd9c66f30a1390916520d561fa07
Irina Tretiakova <irina.tretiakova54@gmail.com>
2025-11-09 19:39:18 +0100

    Added writing the current balance to a file: 1) If the file hasn't been created, create a text file and write a zero balance to it; 2) Read the file contents and output the file contents (current balance) to the console; 3) Added an exception if the file contents are in the wrong format (not a float); 4) After entering a number, added an exception to check the format of the entered number; 5) Added a return to the beginning of the iteration if the format of the entered number is incorrect; 6) Saved the sum of the entered numbers (balance) to a file;

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 import datetime
+from pathlib import Path
+
 #day = input('Enter the date in formate DD.MM.YY ')
-balance = 0
+
+data_file = Path.cwd() / "database.txt"
+if not data_file.exists():
+    print("Database file doesn't exists, creating an empty file with zero balance")
+    with data_file.open(mode="w") as file:
+        file.write("0")
+data_file_content = data_file.read_text()
+try:
+    balance = float(data_file_content)
+except ValueError:
+    print("Could not parse Database file. Please check its content.")
+    print("Quitting program...")
+    quit()
+
+print(f"Current balance: {balance}")
+
 while True:
     action = input("Input positive or negative amount or empty string to quit: ")
     if action == "":
         print("Exit program")
         quit()
 
-    action = int(action)
+    try:
+        action = float(action)
+    except ValueError:
+        print("Please, use the following formate 0.00.")
+        continue
+
     balance = balance + action
+    # Saving new balance into file.
+    with data_file.open(mode="w") as file:
+        file.write(str(balance))
+
+
     if action < 0:
         print(f"Amount {action} is deducted. Actual balance: {balance}")
     else:
         print(f"Amount {action} is added. Actual balance: {balance}")
+
+
+
+
+# if data_file.exists():
+#     with data_file.open(mode="a") as file:
+#         contents = file.write("Happy new year!")
+#         print(f"File{data_file.name} is now open")
+#     print(f"File{data_file.name} is now close")
