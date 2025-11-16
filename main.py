0a7e1ebb7928f17e096d7e48834c30988f67fc1e
Irina Tretiakova <irina.tretiakova54@gmail.com>
2025-11-16 21:01:12 +0100

    Added the ability to write each individual entered transaction to a file. To do this, I added the write_transaction function, which writes each new transaction to a separate line in a text file and calls the get_balance function, which reads all the entered lines from the file, converts them to numeric format, adds them together, and transfers them to the overall balance. I also wrapped the file creation/opening commands and calls to the functions described above in the balance variable.

--- main.py
+++ main.py
@@ -3,49 +3,74 @@ from pathlib import Path
 
 #day = input('Enter the date in formate DD.MM.YY ')
 
-data_file = Path.cwd() / "database.txt"
-if not data_file.exists():
-    print("Database file doesn't exists, creating an empty file with zero balance")
-    with data_file.open(mode="w") as file:
-        file.write("0")
-data_file_content = data_file.read_text()
-try:
-    balance = float(data_file_content)
-except ValueError:
-    print("Could not parse Database file. Please check its content.")
-    print("Quitting program...")
-    quit()
 
-print(f"Current balance: {balance}")
+def get_balance(data_file):
+    """Get current balance.
 
-while True:
-    action = input("Input positive or negative amount or empty string to quit: ")
-    if action == "":
-        print("Exit program")
-        quit()
+    Reads database.txt file, calculates and returns total balance."""
+    total_balance = 0
+    with data_file.open(mode="r") as file_content:
+        lines = file_content.readlines()
 
-    try:
-        action = float(action)
-    except ValueError:
-        print("Please, use the following formate 0.00.")
-        continue
+    for line in lines:
+        try:
+            amount = float(line)
+        except ValueError:
+            print("Could not parse Database file. Please check its content.")
+            print("Quitting program...")
+            quit()
 
-    balance = balance + action
-    # Saving new balance into file.
-    with data_file.open(mode="w") as file:
-        file.write(str(balance))
+        total_balance += amount
+    return total_balance
 
 
-    if action < 0:
-        print(f"Amount {action} is deducted. Actual balance: {balance}")
-    else:
-        print(f"Amount {action} is added. Actual balance: {balance}")
+def write_transaction(data_file, amount):
+    """Write new transaction.
+
+    Function writes new transaction amount to the file as a new line.
+    Function returns updated total balance"""
+    with data_file.open(mode="a") as file_content:
+        file_content.write(str(amount) + "\n")
+    return get_balance(data_file)
+
+
+def main():
+    """Main function."""
+
+    data_file = Path.cwd() / "database.txt"
+    if not data_file.exists():
+        print("Database file doesn't exists, creating an empty file with zero balance")
+        with data_file.open(mode="w") as file:
+            file.write("0\n")
+
+    balance = get_balance(data_file)
+    print(f"Current balance: {balance}")
+
+    while True:
+        action = input("Input positive or negative amount or empty string to quit: ")
+        if action == "":
+            print("Exit program")
+            quit()
+
+        try:
+            amount = float(action)
+        except ValueError:
+            print("Please, use the following formate 0.00.")
+            continue
+
+        # Saving new balance into file.
+        balance = write_transaction(data_file, amount)
+
+        if amount < 0:
+            print(f"Amount {amount} is deducted. Actual balance: {balance}")
+        else:
+            print(f"Amount {amount} is added. Actual balance: {balance}")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
 
-# if data_file.exists():
-#     with data_file.open(mode="a") as file:
-#         contents = file.write("Happy new year!")
-#         print(f"File{data_file.name} is now open")
-#     print(f"File{data_file.name} is now close")
+
